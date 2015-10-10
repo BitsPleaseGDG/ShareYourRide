d454b2e8681208fe0c857e8ede968706e38db6a6
kanta <kanta@csews132.cse.iitk.ac.in>
2015-10-11 01:20:51 +0530

    Added index file and changed some stuff

--- app.js
+++ app.js
@@ -139,7 +139,7 @@ app.connection = connection;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
