c41f147a46e0f35d03cc2ea239df2c67374ef69a
Anand Singh Kunwar <anandkunwar95@gmail.com>
2015-10-10 17:49:34 +0530

    Added basic fb login

--- app.js
+++ app.js
@@ -4,11 +4,54 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var ping = require('./routes/ping');
+
+var config = require('./keys.js')
+var passport = require('passport')
+var FacebookStrategy = require('passport-facebook').Strategy;
+var GoogleStrategy = require('passport-google').Strategy;
+
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+done(null, obj);
+});
+
+
+// var User = mongoose.model('User', {
+//   oauthID: Number,
+//   name: String
+// });
+
+// config
+passport.use(new FacebookStrategy({
+ clientID: config.facebook.clientID,
+ clientSecret: config.facebook.clientSecret,
+ callbackURL: config.facebook.callbackURL
+},
+function(accessToken, refreshToken, profile, done) {
+ process.nextTick(function () {
+   return done(null, profile);
+ });
+}
+));
+
 
 var app = express();
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : config.mysql.host,
+  user     : config.mysql.user,
+  password : config.mysql.password,
+  database : config.mysql.database
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,10 +63,45 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// app.get('/', routes.index);
+// app.get('/ping', routes.ping);
+// app.get('/account', ensureAuthenticated, function(req, res){
+// res.render('account', { user: req.user });
+// });
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/ping', ping);
+app.get('/account', ensureAuthenticated, function(req, res){
+res.render('account', { user: req.user });
+});
+
+
+app.get('/', function(req, res){
+res.render('login', { user: req.user });
+});
+
+app.get('/auth/facebook',
+passport.authenticate('facebook'),
+function(req, res){
+});
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook', { failureRedirect: '/' }),
+function(req, res) {
+ res.redirect('/account');
+});
+app.get('/logout', function(req, res){
+req.logout();
+res.redirect('/');
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,6 +124,13 @@ if (app.get('env') === 'development') {
   });
 }
 
+
+connection.connect();
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+ if (err) throw err;
+
+ console.log('The solution is: ', rows[0].solution);
+});
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
@@ -56,5 +141,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
+function ensureAuthenticated(req, res, next) {
+if (req.isAuthenticated()) { return next(); }
+res.redirect('/')
+}
 
 module.exports = app;
